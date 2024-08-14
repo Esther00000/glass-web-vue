c4d94671eeeb52cd97c7b669fe3168b879706f1f
Esther <fuest8456258@gmail.com>
2024-08-14 14:52:36 +0800

    新增base

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/glass-web-vue/",
   resolve: {
     alias: {
       // alias定義路徑別名
